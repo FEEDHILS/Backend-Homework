e5bbc14ee09e10371ef6be581a7a89a648dc2bd3
des0ager <dmitriy1350077@gmail.com>
2024-12-08 18:54:50 +1000

    2 Задание
    надеюсь

--- ChingChong/main/views.py
+++ ChingChong/main/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(req):
-    return render(req, "index.html", {})
-    # return HttpResponse("Hello World")
+    if req.method == "GET":
+        return render(req, "index.html", {})
+    
+def auth(req):
+    if req.method == "GET":
+        return render(req, "auth.html", {})
+    
+def result(req):
+    if req.method == "POST":
+        data = {'name' : req.POST['fname'] + " " + req.POST['lname']}
+        return render(req, "result.html", data)
