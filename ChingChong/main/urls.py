6bf53f6bafa1b2031d4492313c23e4c804d7f5a9
des0ager <dmitriy1350077@gmail.com>
2024-12-09 16:24:51 +1000

    4  Задание
    Че то такое чувство будто все на соплях держится.

--- ChingChong/main/urls.py
+++ ChingChong/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .models import User
 
 urlpatterns = [
     path('', views.index),
+    path('reg', views.reg),
     path('auth', views.auth),
-    # path('result', views.result),
+    # Чтобы можно было передавать пользователей (их id)
+    path('greet/id/<int:id>', views.greet),
 ]
--- ChingChong/main/views.py
+++ ChingChong/main/views.py
@@ -1,22 +1,52 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import User
 
-# Create your views here.
+# Главная страница (Вход в пользователя).
 def index(req):
     if req.method == "GET":
-        return render(req, "index.html",)
-    if req.method == "POST":
-        return redirect(auth, req.POST)
-    
-def auth(req):
-    if req.method == "GET":
-        return render(req, "auth.html",)
-    if req.method == "POST":
-        print(req.POST)
-        return render(req, "auth.html", req.POST)
+        return render(req, "index.html", {})
 
+# Эндпоинт проверки данных.
+def auth(req):
+    if req.method == "POST":
+        # Проверяем есть ли пользователь с такими данными в БД.
+        login = req.POST['login']
+        password = req.POST['pass']
+        
+        try:
+            user = User.objects.get(login=login, password=password)
+        except User.DoesNotExist:
+            return HttpResponse("Такого пользователя не существует, зарегестрируйтесь.")
+
+        return redirect(greet, user.pk)
+
+# Страница с регистрацией.
+def reg(req):
+    if req.method == "GET":
+        return render(req, "register.html", {})
     
-# def result(req):
-#     if req.method == "POST":
-#         data = {'name' : req.POST['fname'] + " " + req.POST['lname']}
-#         return render(req, "result.html", data)
+    if req.method == "POST":
+        # Создаем пользователя
+        new_user = User()
+        new_user.login = req.POST['login']
+        new_user.name = req.POST['name']
+        new_user.password = req.POST['pass']
+        
+        # Ищем дубликат по логину
+        try:
+            User.objects.get(login=new_user.login)
+        except User.DoesNotExist:
+            new_user.save()
+            return redirect(greet, new_user.pk)
+        
+        return render(req, "register.html", {"error": "Такой логин уже используется."})
+
+
+def greet(req, id):
+    # Ищем пользователя, и если не находим, выводим ошибку.
+    try:
+        name = User.objects.get(id=id).name
+        return HttpResponse("Дарова " + name)
+    except User.DoesNotExist:
+        return HttpResponse("Ошибка, такого пользователя не существует") 
